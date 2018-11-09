5bb4c750a93ef19fea2913dfe65c28c74a497210
Los <Los@LosPrograms.com>
2018-11-08 16:49:31 -0800

    Added another view as well as updated the composer packages

--- App/Services/View.php
+++ App/Services/View.php
@@ -13,10 +13,24 @@ class View
 
     public static function Make ($View, $Vars = [])
     {
+        if ($View == 'JSON')
+        {
+            $JSON = json_encode($Vars);
+            die($JSON);
+        }
+
         $Vars['SESSION'] = Session::GetTheSession();
         $Template = new bladeone\BladeOne(SITE_ROOT.self::$Views, SITE_ROOT.self::$Cache);
         $View = self::$ModuleViews ? self::$ModuleViews.'.'.$View : 'Views.'.$View;
         die($Template->run($View, $Vars));
+    }
+
+    public static function GetHTML ($View, $Vars = [])
+    {
+        $Vars['SESSION'] = Session::GetTheSession();
+        $Template = new bladeone\BladeOne(SITE_ROOT.self::$Views, SITE_ROOT.self::$Cache);
+        $View = self::$ModuleViews ? self::$ModuleViews.'.'.$View : 'Views.'.$View;
+        return $Template->run($View, $Vars);
     }
 }
 ?>
