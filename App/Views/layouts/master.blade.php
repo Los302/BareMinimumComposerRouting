37b507b66d58f5798ba9cd450a9645933678663c
Los <Los@LosPrograms.com>
2018-11-11 23:17:28 -0800

    Replaced php echo tags in the master layout with blade style echo

--- App/Views/layouts/master.blade.php
+++ App/Views/layouts/master.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title><?=SITE_NAME?></title>
-    <base href="<?=SITE_URL?>">
+    <title>{{ SITE_NAME }}</title>
+    <base href="{{ SITE_URL }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     @yield('CSSB4Main')
     <link rel="stylesheet" href="/public/_CSS/main.css" />
@@ -14,16 +14,16 @@
 <body>
 <header>
     <div class="container">
-        <h1><?=SITE_NAME?></h1>
+        <h1>{{ SITE_NAME }}</h1>
     </div>
 </header>
 <nav>
     <div class="container">
-        <a href="<?=SITE_URL?>">Home</a> |
+        <a href="{{ SITE_URL }}">Home</a> |
         @if (!$SESSION->IsLoggedIn())
-        <a href="<?=SITE_URL?>User">Login</a>
+        <a href="{{ SITE_URL }}User">Login</a>
         @else
-        <a href="<?=SITE_URL?>User/Logout">Logout</a>
+        <a href="{{ SITE_URL }}User/Logout">Logout</a>
         @endif
     </div>
 </nav>
@@ -31,7 +31,7 @@
 @yield('Content')
 </section>
 <footer>
-    <div class="copyright">&copy; <?=date('Y')?> Copyright <?=SITE_NAME?></div>
+    <div class="copyright">&copy; {{ date('Y') }} Copyright {{ SITE_NAME }}</div>
 </footer>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="/public/_JS/main.js"></script>
